c937437e5183dccc28056735480f09586879f9ff
hajar6883 <hajarlamtaai25@gmail.com>
2026-01-13 13:30:33 +0100

    updates and test

--- models/SABR.py
+++ models/SABR.py
@@ -6,4 +6,36 @@ def hagan_SABR(K,F,T, alpha, beta, rho, nu, eps=1e-07):
     one_minus_beta = 1.0 - beta
     FK_beta = (F * K) ** (one_minus_beta / 2.0)
 
-    # Time correction common terms (order-T expansion
+    # Time correction common terms (order-T expansion):
+
+    backbone_convexity = ((one_minus_beta ** 2) * alpha ** 2) / (24 * (F * K) ** one_minus_beta)
+    skew_interaction = (rho * beta * nu * alpha) / (4 * FK_beta)
+    smile_curvature = ((2 - 3 * rho ** 2) * nu ** 2) / 24
+
+    #ATM case :
+    if abs(F - K) < eps:
+        vol_atm = (
+            alpha / (F ** one_minus_beta)
+            * (1 + (backbone_convexity + skew_interaction + smile_curvature) * T)
+        )
+        return vol_atm
+    # Non-ATM (general formula)
+
+    # base(backbone) volatility level (CEV scaling)
+    
+    log_FK = np.log(F/K)
+
+    z = (nu / alpha) * FK_beta * log_FK    # log-moneyness scalling var:
+    xz = np.log((np.sqrt(1.0 - 2.0*rho*z + z*z) + z - rho) / (1.0 - rho))     # skew_smile_adj
+
+    # sigma_imp(F,K)
+    base = alpha / FK_beta
+    vol = base * (z / xz) * (1 + (backbone_convexity + skew_interaction + smile_curvature) * T)
+    return vol
+
+
+
+
+
+    
+
